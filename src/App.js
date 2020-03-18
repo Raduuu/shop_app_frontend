f891ea64e6057a9258b3f2ad17f559d35f8fabfe
Radu-Paul PALADE <rpalade@p5ro-port1024.local>
2020-03-18 17:29:48 +0200

    added login and register logic + a main view component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Login from './components/Login'
+import MainView from './components/MainView'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
-function App() {
-    return (
-        <div className="App">
-            <Login></Login>
-        </div>
-    )
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoggedIn: false,
+        }
+    }
+
+    setIsLoggedIn = () => {
+        this.setState({
+            isLoggedIn: !this.state.isLoggedIn,
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Switch>
+                        <Route path="/signup">
+                            <Login type="signup" />
+                        </Route>
+                        <Route path="/login">
+                            <Login type="login" setIsLoggedIn={this.setIsLoggedIn} />
+                        </Route>
+                        <Route path="/">
+                            <MainView setIsLoggedIn={this.setIsLoggedIn}></MainView>
+                        </Route>
+                        <Route
+                            render={({ location }) => {
+                                debugger
+                                return (
+                                    !this.state.isLoggedIn && (
+                                        <Redirect
+                                            to={{
+                                                pathname: '/login',
+                                            }}
+                                        />
+                                    )
+                                )
+                            }}
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
 
 export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Link, withRouter } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
 `
@@ -14,6 +15,38 @@ const StyledInput = styled.input`
     display: block;
     font-size: 24px;
     margin-bottom: 10px;
+`
+
+const StyledError = styled.p`
+    color: red;
+    font-size: 12px;
+`
+
+const StyledTitle = styled.h2`
+    display: block;
+    margin-bottom: 30px;
+    width: 100%;
+`
+
+const StyledLink = styled(Link)`
+    width: 100%;
+    display: block;
+    margin: 10px 0;
+`
+
+const StyledButton = styled.button`
+    color: #fff;
+    text-transform: uppercase;
+    text-decoration: none;
+    background: #ed3330;
+    padding: 20px;
+    border-radius: 5px;
+    display: inline-block;
+    border: none;
+    transition: all 0.4s ease 0s;
+    font-weight: 700;
+    font-size: 16px;
+    cursor: pointer;
 `
 
 class Login extends React.Component {
@@ -27,7 +60,6 @@ class Login extends React.Component {
 
     onSubmit = ev => {
         ev.preventDefault()
-        console.log(this.state)
         const email = this.state.email
         const password = this.state.password
         const body = {
@@ -37,14 +69,21 @@ class Login extends React.Component {
         const headers = {
             'Content-Type': 'application/json',
         }
-
-        fetch('http://localhost:9000/signin', {
+        const { type } = this.props
+        fetch(`http://localhost:9000/${type === 'login' ? 'signin' : 'signup'}`, {
             method: 'POST',
             headers,
             body: JSON.stringify(body),
         })
             .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
+            .then(res => {
+                debugger
+                this.setState({ apiResponse: res })
+                if (JSON.parse(this.state.apiResponse).token && this.props.type === 'login') {
+                    this.props.setIsLoggedIn()
+                    this.props.history.push('/')
+                }
+            })
     }
 
     handleEmailChange = e => {
@@ -57,7 +96,11 @@ class Login extends React.Component {
     render() {
         return (
             <Wrapper>
+                <StyledTitle>{this.props.type === 'login' ? 'Sign in' : 'Register'}</StyledTitle>
                 <form noValidate>
+                    {this.state.apiResponse && (
+                        <StyledError className="error">{JSON.parse(this.state.apiResponse).message}</StyledError>
+                    )}
                     <StyledInput
                         type="text"
                         placeholder="Email"
@@ -68,13 +111,18 @@ class Login extends React.Component {
                         placeholder="Password"
                         onChange={e => this.handlePasswordChange(e)}
                     ></StyledInput>
-                    <button type="submit" onClick={ev => this.onSubmit(ev)}>
-                        Login
-                    </button>
+                    {this.props.type !== 'login' ? (
+                        <StyledLink to="/login">Login</StyledLink>
+                    ) : (
+                        <StyledLink to="/signup">Register</StyledLink>
+                    )}
+                    <StyledButton type="submit" onClick={ev => this.onSubmit(ev)}>
+                        {this.props.type === 'login' ? 'Login' : 'Register'}
+                    </StyledButton>
                 </form>
             </Wrapper>
         )
     }
 }
 
-export default Login
+export default withRouter(Login)
