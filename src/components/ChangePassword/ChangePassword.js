d4946be6cfededb0f2cd0c7b150fb34453b29aff
Radu-Paul PALADE <rpalade@p5ro-port1024.local>
2020-04-02 19:24:04 +0300

    added change password page

---
+++ src/components/ChangePassword/ChangePassword.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import axios from 'axios'
+import Cookie from 'js-cookie'
+
+class PasswordPage extends React.Component {
+    constructor(props) {
+        super(props)
+        // potential security risk
+        this.state = {
+            oldpassword: '',
+            newpassword: '',
+        }
+    }
+
+    handleSubmit = ev => {
+        ev.preventDefault()
+        const token = Cookie.get('token') ? Cookie.get('token') : null
+        const BearerToken = `Bearer ${token}`
+        const headers = {
+            'Content-Type': 'application/json',
+            Authorization: BearerToken,
+        }
+
+        const body = {
+            email: Cookie.get('email'),
+            oldpassword: this.state.oldpassword,
+            newpassword: this.state.newpassword,
+        }
+        axios
+            .post('http://localhost:9000/api/changepassword', body, { headers: headers })
+            .then(res => {
+                this.setState({ success: res.data.message })
+            })
+            .catch(err => {
+                this.setState({ success: '' })
+                console.error(err)
+            })
+    }
+
+    render() {
+        console.log('this.state', this.state)
+        return (
+            <form noValidate>
+                <p>{this.state.success}</p>
+                <input
+                    name="oldpassword"
+                    type="password"
+                    placeholder="Old Password"
+                    onChange={ev => this.setState({ oldpassword: ev.target.value })}
+                />
+                <input
+                    name="newpassword"
+                    type="password"
+                    placeholder="New Password"
+                    onChange={ev => this.setState({ newpassword: ev.target.value })}
+                />
+                <button type="submit" onClick={ev => this.handleSubmit(ev)}>
+                    Submit
+                </button>
+            </form>
+        )
+    }
+}
+
+export default PasswordPage
--- src/components/ChangePassword/PasswordPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const PasswordPage = () => {}
-
-export default PasswordPage
