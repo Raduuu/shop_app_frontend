8151d4ae8ef7f317c85ef6236ba6dd611b55a045
Radu-Paul PALADE <rpalade@192-168-0-125.rdsnet.ro>
2020-04-18 18:01:08 +0300

    added filter select

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from './Product'
+import { get } from '../../utils/utils'
 
-const ProductsList = ({ products, updateProducts, editProduct, isAdmin, setCartProducts }) => {
-    return products.map((product) => (
-        <Product
-            updateProducts={updateProducts}
-            editProduct={editProduct}
-            key={product._id}
-            product={product}
-            isAdmin={isAdmin}
-            setCartProducts={setCartProducts}
-        />
-    ))
+export default class ProductsList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            categories: null,
+        }
+    }
+
+    componentDidMount() {
+        get('api/category', (res) => {
+            res && this.setState({ categories: res.data.data })
+        })
+    }
+
+    render() {
+        const { products, updateProducts, editProduct, isAdmin, setCartProducts, handleSelect } = this.props
+
+        return (
+            <>
+                <select onChange={(ev) => handleSelect(ev.target.value)}>
+                    <option>all</option>
+                    {this.state.categories &&
+                        this.state.categories.map((category, index) => <option key={index}>{category.name}</option>)}
+                </select>
+                {products.map((product) => (
+                    <Product
+                        updateProducts={updateProducts}
+                        editProduct={editProduct}
+                        key={product._id}
+                        product={product}
+                        isAdmin={isAdmin}
+                        setCartProducts={setCartProducts}
+                    />
+                ))}
+            </>
+        )
+    }
 }
-
-export default ProductsList
