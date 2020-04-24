58dc1db4b2cf0f315e7140bbcf4f4b8799776cb7
Radu-Paul PALADE <p.radupaul@gmail.com>
2020-04-24 13:37:16 +0300

    added filter bar

--- src/components/Products/FilterBar.js
+++ src/components/Products/FilterBar.js
@@ -1,5 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const FlexWrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
 export default class FilterBar extends React.Component {
-    render() {}
+    render() {
+        const { handleSearch, handleSelect, categories } = this.props
+        return (
+            <FlexWrapper>
+                <select onChange={(ev) => handleSelect(ev.target.value)}>
+                    <option>all</option>
+                    {categories && categories.map((category, index) => <option key={index}>{category.name}</option>)}
+                </select>
+                <input
+                    placeholder="Search..."
+                    type="text"
+                    name="search"
+                    onChange={(ev) => handleSearch(ev.target.value)}
+                />
+            </FlexWrapper>
+        )
+    }
 }
