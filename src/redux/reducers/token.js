6a6c2742aa8c7215296369256362eba7655897cd
Radu-Paul PALADE <p.radupaul@gmail.com>
2020-05-07 18:48:05 +0300

    changed token ( further to be deleted)

--- src/redux/reducers/token.js
+++ src/redux/reducers/token.js
@@ -3,9 +3,9 @@ import { SET_TOKEN, REMOVE_TOKEN } from '../actions/token'
 const token = (state = [], action) => {
     switch (action.type) {
         case SET_TOKEN:
-            return [...state, { token: action.token }]
+            return [...state, action.token]
         case REMOVE_TOKEN:
-            return [...state, { token: undefined }]
+            return state
         default:
             return state
     }
