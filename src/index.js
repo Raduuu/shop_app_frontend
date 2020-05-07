c9080ceb450bf7bd1cf40cede8dd109dbcd9c53b
Radu-Paul PALADE <p.radupaul@gmail.com>
2020-05-07 18:48:38 +0300

    removed store creation from react and created it in redux layer + saga

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './redux/reducers'
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+import store from './redux/reducers'
 
 ReactDOM.render(
     <Provider store={store}>
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,24 @@
-import { combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import token from './token'
+import { saga, reducer as products } from '../reducers/products'
 
-export default combineReducers({
+export const rootReducer = combineReducers({
     token,
+    products,
 })
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+// mount it on the Store
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(sagaMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
+)
+
+// then run the saga
+sagaMiddleware.run(saga)
+
+export default store
