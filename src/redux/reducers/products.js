d9eaee5885d4b312f9c36c2ebf01d81dec3dbbcc
Radu-Paul PALADE <p.radupaul@gmail.com>
2020-05-08 16:39:23 +0300

    products action creator + reducer

--- src/redux/reducers/products.js
+++ src/redux/reducers/products.js
@@ -1,6 +1,13 @@
 import { call, put, takeLatest } from 'redux-saga/effects'
-import { CREATE_PRODUCT_REQUESTED, CREATE_PRODUCT_SUCCEEDED, CREATE_PRODUCT_FAILED } from '../constants'
-import { post } from '../utils/utils'
+import {
+    CREATE_PRODUCT_REQUESTED,
+    CREATE_PRODUCT_SUCCEEDED,
+    CREATE_PRODUCT_FAILED,
+    GET_PRODUCTS_REQUESTED,
+    GET_PRODUCTS_SUCCEEDED,
+    GET_PRODUCTS_FAILED,
+} from '../constants'
+import { post, get } from '../utils/utils'
 
 const reducer = (state = {}, action) => {
     switch (action.type) {
@@ -8,11 +15,20 @@ const reducer = (state = {}, action) => {
             return { ...state, [action.payload.id]: action.payload, success: true }
         case CREATE_PRODUCT_FAILED:
             return { ...state, success: false }
+        case GET_PRODUCTS_SUCCEEDED:
+            return { ...state, products: action.payload.data, count: action.payload.count }
+        case GET_PRODUCTS_FAILED:
+            return state
         default:
             return state
     }
 }
 
+// selectors
+const selectProducts = (state) => state.products.products
+const selectCount = (state) => state.products.count
+
+// sagas
 function* createProduct(action) {
     try {
         yield call(post, action.payload, 'api/product')
@@ -22,8 +38,18 @@ function* createProduct(action) {
     }
 }
 
-function* saga() {
-    yield takeLatest(CREATE_PRODUCT_REQUESTED, createProduct)
+function* getProducts() {
+    try {
+        const products = yield call(get, 'api/product/')
+        yield put({ type: GET_PRODUCTS_SUCCEEDED, payload: products })
+    } catch {
+        yield put({ type: GET_PRODUCTS_FAILED })
+    }
 }
 
-export { saga, reducer }
+function* saga() {
+    yield takeLatest(CREATE_PRODUCT_REQUESTED, createProduct)
+    yield takeLatest(GET_PRODUCTS_REQUESTED, getProducts)
+}
+
+export { saga, reducer, selectProducts, selectCount }
